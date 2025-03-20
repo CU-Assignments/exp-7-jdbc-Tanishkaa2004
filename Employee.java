0191846a2a2ff599e1ab8dea4dd85f7b46295e91
Tanishka Singh <157089030+Tanishkaa2004@users.noreply.github.com>
2025-03-20 11:57:22 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,34 @@
+import java.sql.*;
+
+public class EmployeeData {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/your_database";
+        String user = "your_username";
+        String password = "your_password";
+
+        try {
+            // Load MySQL JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish connection
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Employee");
+
+            // Display records
+            while (rs.next()) {
+                int empID = rs.getInt("EmpID");
+                String name = rs.getString("Name");
+                double salary = rs.getDouble("Salary");
+                System.out.println("EmpID: " + empID + ", Name: " + name + ", Salary: " + salary);
+            }
+
+            // Close resources
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
